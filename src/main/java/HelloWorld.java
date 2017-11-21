a23838914b1f751262a0b44444126a8811fa55e7
Anton Kulish <antonkulish8@gmail.com>
2017-11-21 19:35:59 +0200

    Classwork

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,22 @@ public class HelloWorld {
 
         Fibonacci fibonacciNumbers = new Fibonacci();
         fibonacciNumbers.printFibonacciNumbers(12);
+
+        Person person1 = new Person(12, "Kolya");
+        Person person2 = new Person(18, "David");
+        Person person3 = new Person(12, "Kolya");
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.equals(person3));
+        System.out.println(person1.hashCode());
+
+
+        try {
+            int a = 100 / 0;
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+        }
     }
 }
